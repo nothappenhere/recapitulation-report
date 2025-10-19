c5f1409c80448764d5823ef46488f7893de86bd6
nothappenhere <muhammad.rizky15@mhs.itenas.ac.id>
2025-10-19 12:48:07 +0700

    Setup build

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,7 @@
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +9,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@schemas": path.resolve(__dirname, "./schemas"),
     },
   },
 });
